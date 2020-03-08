34f24fa73853cf4ba6f132889f38e4d9367dc318
Aleksandr Kuzminsky <aleksandr.kuzminsky@gmail.com>
2020-03-08 13:03:01 -0700

    Read user SSH keys from attrnibutes
    See kitchen.yml for examples.

--- files/restore_certificates.sh
+++ files/restore_certificates.sh
@@ -7,5 +7,11 @@ backup_copy=$(twindb-backup ls --type files | grep _etc_letsencrypt- | sort -t /
 if ! test -z "$backup_copy"
 then
   twindb-backup restore file --dst /var/tmp/letsencrypt "$backup_copy"
-  cp -R /var/tmp/letsencrypt/etc/letsencrypt /etc/
+  restored_dir=/var/tmp/letsencrypt/etc/letsencrypt
+  if test -d $restored_dir
+  then
+    cp -R /var/tmp/letsencrypt/etc/letsencrypt /etc/
+  else
+    echo "WARNING: $restored_dir doesn't exist"
+  fi
 fi
