7686a7a68c5a27e99cd8e9cd059006a9aab3b4bb
Aleksandr Kuzminsky <aleksandr.kuzminsky@gmail.com>
2020-03-10 20:28:58 -0700

    Move awscli to package recipe
    awscli is a prerequisite for users.rb as well as chef-server.rb

--- recipes/chef-server.rb
+++ recipes/chef-server.rb
@@ -1,27 +1,5 @@
 raise "You must accept Chef license by setting attribute node['chef-server']['accept_license'] to true" unless node['chef-server']['accept_license']
 
-directory '/root/.aws' do
-    owner 'root'
-    group 'root'
-    mode '0700'
-end
-
-aws_config_file = '/root/.aws/config'
-template aws_config_file do
-    source 'aws_config.erb'
-    sensitive true
-    owner 'root'
-    group 'root'
-    mode '600'
-    variables(
-        aws_access_key_id: node['chef-server']['aws_access_key_id'],
-        aws_secret_access_key: node['chef-server']['aws_secret_access_key'],
-        aws_region: node['chef-server']['aws_region']
-    )
-end
-python_package 'awscli'
-python_package 'boto3'
-
 execute 'restore_chef_server' do
     command "/usr/local/bin/restore_chef_server"
     not_if  "chef-server-ctl org-show #{node['chef-server']['org_short_name']}"
